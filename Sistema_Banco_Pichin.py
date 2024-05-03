287eed55190c0df69cfaa892fabcdc7208923625
Hugo <100495717@alumnos.uc3m.es>
2024-05-03 21:02:03 +0200

    yeah baby

--- Sistema_Banco_Pichin.py
+++ Sistema_Banco_Pichin.py
@@ -1,46 +1,64 @@
 from MFIS_Read_Functions import readFuzzySetsFile, readRulesFile, readApplicationsFile
-from MFIS_Classes import FuzzySetsDict, RuleList, Application
+from MFIS_Classes import FuzzySet, FuzzySetsDict
+
 import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
 
-def logica_borrosa(RuleList, FuzzySetsDict, Application):
+def logica_borrosa(RuleList, FuzzySetsDict, Application,RisksDict):
+    reglita = 0
+
+    s_antigua = 0
+
     for regla in RuleList:
+
+
+
+        print("Regla:", reglita, "\n")
         antecedentes = regla.antecedent
         consecuente = regla.consequent
+        print(antecedentes)
+        print(consecuente)
 
         s = 1
 
         for antecedente in antecedentes:
 
             fuzzy = FuzzySetsDict[antecedente]
-
-            grado_pertenencia = fuzz.interp_membership(fuzzy.x,fuzzy.y, Application.data)
+            antecedent_value = next(data[1] for data in Application.data if data[0] == fuzzy.var)
+            print(antecedent_value)
+            grado_pertenencia = fuzz.interp_membership(fuzzy.x,fuzzy.y, antecedent_value)
+            print(grado_pertenencia)
 
             s = min(s,grado_pertenencia)
+            print("S:", s)
 
-        consecuente_borroso = FuzzySetsDict[consecuente]
-        consecuente_borroso.memDegree = max(consecuente_borroso.memDegree, s)
-    return FuzzySetsDict
+        RisksDict[consecuente] = max(s,s_antigua)
+        s_antigua = s
 
-FuzzySetsDict = readFuzzySetsFile("InputVarSets.txt")
 
-RuleList = readRulesFile("Rules.txt")
 
-Applications = readApplicationsFile("Application.txt")
+
+
+
+
+    return RisksDict
+
+FuzzySetsDict = readFuzzySetsFile('Files/InputVarSets.txt')
+
+RuleList = readRulesFile()
+
+Applications = readApplicationsFile()
+RisksDict = readFuzzySetsFile("Files/Risks.txt")
 
 resultados = open("Resultados.txt", "w")
 
 for application in Applications:
-
-    inferencia = logica_borrosa(RuleList,FuzzySetsDict, application)
-
-    resultados.write("Solicitud" + application.appId + "\n")
-    for id, fuzzy in resultados.items():
-        if fuzzy.memDegree < 0.5:
-            resultados.write("Solicitud de préstamo" + str(id) + ": Denegada\n")
-        elif fuzzy.memDegree >= 0.5:
-            resultados.write("Solicitud de préstamo" + str(id) + ": Aceptada\n")
+    resultados.write("Solicitud número" + application.appId + "\n")
+    salidas = logica_borrosa(RuleList,FuzzySetsDict, application,RisksDict)
+    resultados.write("LowR: " + str(salidas["Risk=LowR"]))
+    resultados.write("MediumR: " + str(salidas["Risk=MediumR"]))
+    resultados.write("HighR: " + str(salidas["Risk=HighR"]) + "\n")
 
 resultados.close()
